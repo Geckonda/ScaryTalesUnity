e8e0627b27130ac2227dac6d62595fe70d3d04b6
Geckonda <aquariusstd@yandex.ru>
2025-07-05 16:27:35 +0400

    feat(CoroutineUtils: добавил класс ожиданий корутин)

---
+++ Assets/Scripts/Utilities/CoroutineUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Utilities
+{
+    public static class CoroutineUtils
+    {
+        public static Task WaitForCoroutine(MonoBehaviour owner, IEnumerator coroutine)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            owner.StartCoroutine(Run());
+
+            IEnumerator Run()
+            {
+                yield return coroutine;
+                tcs.SetResult(true);
+            }
+
+            return tcs.Task;
+        }
+    }
+
+}
