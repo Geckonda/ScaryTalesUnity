5f59259ce95cf75d024e6f3ab3b67d3c5b14d996
Geckonda <aquariusstd@yandex.ru>
2025-10-23 23:46:31 +0400

    feat(Cursor): добавил курсор

---
+++ Assets/Scripts/UIEntities/Components/Cursors/CardHoverCursor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.UIEntities.Components.Cursors
+{
+    public class CardHoverCursor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            // Используем менеджер если есть, иначе старый код
+            if (CursorManager.Instance != null)
+                CursorManager.Instance.SetPointerCursor();
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            // Возвращаем курсор по умолчанию через менеджер
+            if (CursorManager.Instance != null)
+                CursorManager.Instance.SetDefaultCursor();
+            else
+                Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        }
+    }
+}
---
+++ Assets/Scripts/UIEntities/Components/Cursors/CursorManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.UIEntities.Components.Cursors
+{
+    public class CursorManager : MonoBehaviour
+    {
+        public static CursorManager Instance;
+
+        [Header("Курсоры")]
+        public Texture2D defaultCursor;
+        public Texture2D pointerCursor;
+        public Vector2 hotSpot = Vector2.zero;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                SetDefaultCursor();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void SetDefaultCursor()
+        {
+            Cursor.SetCursor(defaultCursor, hotSpot, CursorMode.Auto);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        public void SetPointerCursor()
+        {
+            Cursor.SetCursor(pointerCursor, hotSpot, CursorMode.Auto);
+        }
+    }
+}
